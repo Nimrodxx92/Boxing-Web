3c0e7e7ce6d5dae65b5fb60269886bcde2e5ef05
Cristian Monsalvo <cnmonsalvo@gmail.com>
2024-07-11 18:49:16 -0300

    login register

--- client/src/client-views/about.jsx
+++ client/src/client-views/about.jsx
@@ -1,34 +1,38 @@
 import aboutImg from "../assets/0000.jpg";
+import boxerAbout from "../assets/boxer-about.jpg";
 import TitleComponent from "../components/TitleComponent";
 import data from "../json/titles.json";
 
 const About = () => {
+  const skills = data.about.clubInfo.skills;
   return (
     <>
-      <section className="">
-        <TitleComponent
-          title={data.about.title}
-          description={data.about.description}
-        />
-      </section>
+      <TitleComponent
+        title={data.about.title}
+        description={data.about.description}
+      />
       <section className="about__container">
         <div className="firts__about">
           <img src={aboutImg} alt="img" />
           <div className="title-firts-about">
-            <h2>Nuestro Club</h2>
-            <p>
-              Nuestro gimnasio es más que una escuela de boxeo: es un club, una
-              fraternidad y, para muchos, un estilo de vida. Guiamos a nuestros
-              estudiantes hacia la excelencia a través del desafío y el apoyo
-              inquebrantable y la atención al detalle.
-            </p>
-            <p>Ubicado en Barrio Marina, Castelar Sur.</p>
+            <h2>{data.about.clubInfo.title}</h2>
+            <p>{data.about.clubInfo.description}</p>
             <div className="skills-entrenamiento">
-              <h3>Mejor Entrenamiento</h3>
-              <h3>Mejores Precios</h3>
-              <h3>Equipo de Calidad</h3>
+              {skills.map((skill, index) => (
+                <div className="countainer-skills" key={index}>
+                  <h3>{skill.name}</h3>
+                  <p>{skill.description}</p>
+                </div>
+              ))}
             </div>
           </div>
+        </div>
+      </section>
+      <section className="subabout_container">
+        <div className="sub-img-about">
+          <img src={boxerAbout} alt="" />
+          <h3>{data.about.mision}</h3>
+          <h3>{data.about.vision}</h3>
         </div>
       </section>
     </>
